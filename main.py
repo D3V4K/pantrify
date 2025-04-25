3d371a20f511bc8d4c27f94316cbcf65cff71887
D3V4K <64558845+D3V4K@users.noreply.github.com>
2025-04-24 18:51:46 -0700

    Made basic api stuff

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+@app.get("/generate-ingredient")
+def generate_ingredient(ingredient1: str, ingredient2: str):
+    return {"third_ingredient": "sigma"}
